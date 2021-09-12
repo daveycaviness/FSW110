e642ebaf51e50a3b60c00728c5e409b211caa312
daveycaviness <85713610+daveycaviness@users.noreply.github.com>
2021-09-11 21:33:14 -0400

    Add files via upload
    week 6 assignments

---
+++ Week6/eventsWorkbook/event-workbook/mousemove/app.js
@@ -0,0 +1,7 @@
+let boxNode = document.getElementsByClassName("red-box");
+let boxArr = Array.from(boxNode);
+let myBox = boxArr[0];
+
+myBox.addEventListener("mousemove", function(e) {
+    myBox.textContent = "This is the mouse coordinates: X=" + e.clientX + "/ Y=" + e.clientY;
+});
---
+++ Week6/eventsWorkbook/event-workbook/submit/app.js
@@ -0,0 +1,12 @@
+// The "submit" event refreshes your browser by default
+// use "e.preventDefault()" in the first line of your callback function to prevent this
+// remember to pass "e" into the callback function
+
+let form = document.getElementById("submit-me");
+let nameInput = form.elements["name"];
+let ageInput = form.elements["age"];
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Name: " + nameInput.value + " / Age: " + ageInput.value);
+});
